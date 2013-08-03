512a550170678e6792220c348d176b1a4d606be4
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-08-04 01:41:52 +0200

    fixes

--- app/assets/javascripts/views/points_view.js
+++ app/assets/javascripts/views/points_view.js
@@ -14,18 +14,23 @@ Mycollecto.PointsView = Em.View.extend({
       });
     }, 500);
 
-    $('.handle').on('click', function(event) {
+    // Point list UX events
+
+    var handle = $('.handle');
+    handle.on('click', function(event) {
       if ($('body').hasClass('no-point-list')) {
         $('body').removeClass('no-point-list');
       } else {
         $('body').addClass('no-point-list');
       }
     });
-    $$('.handle').swipeDown(function() {
+
+    var swipeHandle = $$('.handle'); // cant use the same selector because QuOJs has its own
+    swipeHandle.swipeDown(function() {
       $('body').addClass('no-point-list');
     });
 
-    $$('.handle').swipeUp(function() {
+    swipeHandle.swipeUp(function() {
       $('body').removeClass('no-point-list');
     });
 
