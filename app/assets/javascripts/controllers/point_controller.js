a0b28a0b9acf59e0b03110ee52706571e1fd95d9
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-08-25 20:33:04 +0200

    fix itinerary

--- app/assets/javascripts/controllers/point_controller.js
+++ app/assets/javascripts/controllers/point_controller.js
@@ -9,23 +9,9 @@ Mycollecto.PointController = Em.ObjectController.extend({
 
   findItinirary: function() {
     var controller   = this;
-    var geocoder     = new google.maps.Geocoder();
     var currentPos   = controller.get('controllers.points').currentUserPosition.latLng;
-    var pointLatLng  = new google.maps.LatLng(controller.get('x'), controller.get('y'));
-
-    geocoder.geocode({'latLng': pointLatLng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          debugger
-          var pointAddress = results[0].formatted_address;
-          window.location  = 'http://maps.apple.com/?daddr='+pointAddress+'&saddr='+currentPos.mb+','+currentPos.nb;
-        } else {
-          alert('No results found');
-        }
-      } else {
-        alert('Geocoder failed due to: ' + status);
-      }
-    });
+    var pointAddress = controller.get('content.address_nl');
+    window.location  = 'http://maps.apple.com/?daddr='+pointAddress+'&saddr='+currentPos.lat+','+currentPos.lng;
     mixpanel.track("Find Itinerary");
   }
 });
