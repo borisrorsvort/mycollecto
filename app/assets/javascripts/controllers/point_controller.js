8646d095a5d216a419f20cfd8f7270924809b0a2
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-08-26 23:38:03 +0200

    Optimize itinerary using formatted address

--- app/assets/javascripts/controllers/point_controller.js
+++ app/assets/javascripts/controllers/point_controller.js
@@ -10,7 +10,7 @@ Mycollecto.PointController = Em.ObjectController.extend({
   findItinirary: function() {
     var controller   = this;
     var currentPos   = controller.get('controllers.points').currentUserPosition.latLng;
-    var pointAddress = controller.get('content.addressNl');
+    var pointAddress = controller.get('content.formatted_address');
     window.location  = 'http://maps.apple.com/?daddr='+pointAddress+'&saddr='+currentPos.lat+','+currentPos.lng;
     mixpanel.track("Find Itinerary");
   }
