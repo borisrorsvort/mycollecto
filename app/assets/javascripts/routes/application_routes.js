eddc76255f848e88d57f9f751920cf36f74992a8
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-08-27 21:15:37 +0200

    fix routing

--- app/assets/javascripts/routes/application_routes.js
+++ app/assets/javascripts/routes/application_routes.js
@@ -1,6 +1,6 @@
 Mycollecto.Router.map(function() {
   this.route('about'),
-  this.resource('points', { path: "points" }, function() {
+  this.resource('points', { path: "/" }, function() {
     this.resource('point', {path: ':point_id'});
   });
 });
