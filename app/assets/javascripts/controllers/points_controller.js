8fef3eafca3eda55ee541ab8c5c90a22ffdc8bcf
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-10-11 00:03:06 +0200

    first almost working prototype

--- app/assets/javascripts/controllers/points_controller.js
+++ app/assets/javascripts/controllers/points_controller.js
@@ -13,11 +13,12 @@ Mycollecto.PointsController = Em.ArrayController.extend({
   initMap: function(){
 
     var controller          = this;
+
+    controller.set("map", L.map('map'));
     var currentUserPosition = controller.get('currentUserPosition');
-    var map                 = L.map('map');
+    var map                 = controller.get('map');
 
     var layer = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/110494/256/{z}/{x}/{y}.png', {
-      // attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       key: '92e5866dcc9e47179553d1c6ae09d4c9',
       detectRetina: true,
       maxZoom: 18,
@@ -33,39 +34,44 @@ Mycollecto.PointsController = Em.ArrayController.extend({
     controller.set("path",path);
     controller.set("map", map);
 
-    function onLocationFound(e) {
-
-      var myIcon = L.divIcon({
-        html: '<i class="icon-user"/>',
-        className: 'marker-custom marker-custom-user'
-      });
-
-      var user = L.marker(e.latlng, {
-        icon: myIcon
-      }).addTo(map);
-
-      currentUserPosition.set('latLng', e.latlng);
-      currentUserPosition.set('latitude', e.latlng.lat);
-      currentUserPosition.set('longitude', e.latlng.lng);
-      controller.get("path").set("origin", e.latlng);
-      map.setView( e.latlng, 17, {animate: true} );
-      Mycollecto.Point.find({latitude: e.latlng.lat, longitude: e.latlng.lng, size: 20}).then(function(points){
-        // Feed the content prop with the points
-        controller.set('content', points);
-        // And Redirect to closest point
-        controller.transitionToRoute('point', points.objectAt(0));
-      });
-
-    }
-
-    function onLocationError(e) {
+    map.locate({maximumAge: 2000}).on('locationfound', function(e) {
+      controller.setupUserPosition(e, currentUserPosition, map)
+      controller.setupMapWithPoints(e, map, controller);
+    })
+    .on('locationerror', function() {
       console.log("Location error");
-      controller.set('content', Mycollecto.Point.find());
-    }
-
-    map.locate({maximumAge: 2000});
+      controller.set('content', Mycollecto.Point.find({size: 204}));
+    });
   },
 
+  setupUserPosition: function(e, currentUserPosition, map) {
+    var myIcon = L.divIcon({
+      html: '<i class="icon-user"/>',
+      className: 'marker-custom marker-custom-user'
+    });
+
+    var userMarker = L.marker(e.latlng, {
+      icon: myIcon
+    }).addTo(map);
+
+    this.get('currentUserPosition').setProperties({
+      marker: userMarker,
+      markerIcon: myIcon,
+      latLng: e.latlng,
+      latitude: e.latlng.lat,
+      longitude: e.latlng.lng
+    });
+  },
+
+  setupMapWithPoints: function(e, map, controller) {
+    map.setView( e.latlng, 17, {animate: true} );
+    Mycollecto.Point.find({latitude: e.latlng.lat, longitude: e.latlng.lng, size: 204}).then(function(points){
+      // Feed the content prop with the points
+      controller.set('content', points);
+      // And Redirect to closest point
+      controller.transitionToRoute('point', points.objectAt(0));
+    });
+  },
 
   invalidateMapSize: function() {
     var controller = this;
--- app/assets/javascripts/views/searchbar_input_view.js
+++ app/assets/javascripts/views/searchbar_input_view.js
@@ -3,13 +3,52 @@ Mycollecto.SearchbarInput = Ember.TextField.extend({
     this.interpretKeyEvents(event);
     var controller = this.get('controller').get('controllers.points');
     if (e.keyCode == 13) {
-      Mycollecto.Point.find({address: this.get('value'), size: 20}).then(function(points){
-        // Feed the content prop with the points
-        controller.set('content', points);
-        // And Redirect to closest point
-        var point = points.objectAt(0);
-        controller.transitionToRoute('point', point);
-      });
+      var customAddress = this.get('value');
+      $('#map').spin();
+      $.ajax({
+        url: 'http://beta.geocoding.cloudmade.com/v3/92e5866dcc9e47179553d1c6ae09d4c9/api/geo.location.search.2',
+        type: 'GET',
+        dataType: 'jsonp',
+        data: {
+          format: 'json',
+          source: 'OSM',
+          enc: 'UTF-8',
+          limit: 10,
+          locale: 'en',
+          q: customAddress + ' Bruxelles, Belgique'
+        }
+      }).then(doneCallbacks, failCallbacks)
+
+      function doneCallbacks(data) {
+        // var currentPos  = controller.get('controllers.points').currentUserPosition.latLng;
+        var placeLat = data.places[0].position.lat;
+        var placeLong = data.places[0].position.lon;
+        var newlatlng = new L.LatLng(placeLat, placeLong);
+        controller.get('currentUserPosition.marker').setLatLng(newlatlng);
+        controller.get('currentUserPosition').setProperties({
+          latLng: newlatlng,
+          latitude: placeLat,
+          longitude: placeLong
+        });
+
+        Mycollecto.Point.find({address: customAddress+' Bruxelles, Belgique', size: 204})
+        .then(function(points){
+          // Feed the content prop with the points
+          controller.set('content', points);
+          // And Redirect to closest point
+          var point = points.objectAt(0);
+          controller.transitionToRoute('point', point);
+        });
+
+        $('#map').spin(false);
+        $('.searchbar .btn').first().toggleClass('hidden');
+        $('.searchbar input').first().toggleClass('hidden').blur();
+
+      }
+
+      function failCallbacks (argument) {
+        alert('Failed to locate the place');
+      }
     }
   }
 });
