c6536186b8dee18d10d99f9ccf2e37af11e39d0b
Christophe <christophe.belpaire@gmail.com>
2013-12-09 17:17:22 +0100

    setup the controllers.points.targetPosition in the setupController of point controller instead of observe latitude,longitude of poin

--- app/assets/javascripts/routes/point_route.js
+++ app/assets/javascripts/routes/point_route.js
@@ -2,5 +2,18 @@
 Mycollecto.PointRoute = Ember.Route.extend({
   model: function (params) {
     return Mycollecto.Point.find(params.point_id);
+  },
+
+  setupController: function(controller, model) {
+  	var x = model.get('latitude');
+    var y = model.get('longitude');
+    var newlatlng = new L.LatLng(x, y);
+    controller.get("controllers.points.targetPosition").setProperties({
+      latLng: newlatlng,
+      latitude: x,
+      longitude: y
+    });
+    controller.set('model', model);
   }
+
 });
