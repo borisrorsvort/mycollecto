678a7859b35a0c10855d7e0d0a5b5c4a5a6b7dfd
Boris Rorsvort <boris.rorsvort@gmail.net>
2013-12-14 11:54:58 +0100

    Fix about page

--- app/assets/javascripts/controllers/about_controller.js
+++ app/assets/javascripts/controllers/about_controller.js
@@ -1,7 +1,9 @@
 /*global Mycollecto, Ember*/
 Mycollecto.AboutController = Ember.Controller.extend({
   needs: ['points'],
-  switchToPoints: function () {
-    this.transitionToRoute('points');
+  actions : {
+    switchToPoints: function () {
+      this.transitionToRoute('points');
+    }
   }
 });
