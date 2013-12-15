f70b1b787ed25b0a7fbf84f2156a7aa8da0f1788
Boris Rorsvort <boris.rorsvort@gmail.net>
2013-12-15 23:19:40 +0100

    Add some titles

--- app/assets/javascripts/routes/point_route.js
+++ app/assets/javascripts/routes/point_route.js
@@ -1,11 +1,18 @@
-/*global Mycollecto, Ember*/
+/*global Mycollecto, Ember, L, $, document*/
 Mycollecto.PointRoute = Ember.Route.extend({
+
   model: function (params) {
     return Mycollecto.Point.find(params.point_id);
   },
 
-  setupController: function(controller, model) {
-  	var x = model.get('latitude');
+  afterModel: function (model, transition) {
+    if (model) {
+      $(document).attr('title', 'Mycollecto - Point Collecto Bruxelles: ' + model.get('fullName') + ' - ' + model.get('id'));
+    }
+  },
+
+  setupController: function (controller, model) {
+    var x = model.get('latitude');
     var y = model.get('longitude');
     var newlatlng = new L.LatLng(x, y);
     controller.get("controllers.points.targetPosition").setProperties({
--- app/assets/javascripts/routes/points_index_routes.js
+++ app/assets/javascripts/routes/points_index_routes.js
@@ -1,5 +1,8 @@
-/*global Mycollecto, Ember*/
+/*global Mycollecto, Ember, $, document*/
 Mycollecto.PointsIndexRoute = Ember.Route.extend({
+  activate: function () {
+    $(document).attr('title', 'Mycollecto - Liste des Points Taxi Collecto Bruxelles');
+  },
   renderTemplate: function () {
     this.render('points_index');
   },
