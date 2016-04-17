6c395c0313c228fdb339a89bc98fcd2bbfc1a604
Boris Rorsvort <boris.rorsvort@gmail.com>
2016-04-17 21:24:47 +0200

    Tweak config for deploy

--- config/environment.js
+++ config/environment.js
@@ -39,12 +39,6 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    // ENV.APP.LOG_BINDINGS = true;
     ENV.mixpanel = {
       enabled: true,
       LOG_EVENT_TRACKING: true,
