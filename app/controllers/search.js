fe340cd2739043652664e34657106ff7bb1a18c3
Boris Rorsvort <boris.rorsvort@gmail.com>
2016-04-23 14:24:09 +0200

    Add recent searches with local storage fixes #4

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,14 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  adressesSortingDesc: ['date:desc'],
+  sortedAdresses: Ember.computed.sort('content', 'adressesSortingDesc'),
+  hasSortedAdresses: Ember.computed.notEmpty('sortedAdresses'),
   location: Ember.inject.service(),
+  adressFromMap: function (e) {
+    return e.geometry !== undefined;
+  },
   actions: {
     updatePositions (e) {
+      let lat = this.adressFromMap(e) ? e.geometry.location.lat() : e.latitude;
+      let lng = this.adressFromMap(e) ? e.geometry.location.lng() : e.longitude;
+      var store = this.store;
+
       this.set('location.loading', true);
-      let lat = e.geometry.location.lat();
-      let lng = e.geometry.location.lng();
       this.get('location').updatePositions({latitude: lat, longitude: lng});
       this.get('mixpanel').trackEvent('Search complete', {q: e.formatted_address});
+
+      store.query('adress', {filter: { formatted_address: e.formatted_address }}).then(function(query) {
+        if (query.get('content').length === 0) {
+          let newAdress = store.createRecord('adress', {
+            latitude: lat,
+            longitude: lng,
+            formatted_address: e.formatted_address,
+            date: new Date().getTime()
+          });
+          newAdress.save();
+        }
+      });
+
       this.transitionToRoute('index');
     }
   }
